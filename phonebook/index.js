53c77315e32507b3cb2f7a9fc7b63fb96657e77e
Sean Yeo <seansqyeo@gmail.com>
2020-07-09 21:20:48 -0700

    finish 3.5

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
       name: 'Arto Hellas',
@@ -42,11 +44,35 @@ app.get('/info', (request, response) => {
         <p>${date}</p>`
         )
 })
+
 app.delete('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
 })
+
+app.post('/api/persons', (request, response) => {
+    const id = Math.floor((Math.random()*1000)+1)
+    const body = request.body
+
+    // console.log(body)
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'content is missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: id,
+    }
+    
+    persons = persons.concat(person)
+    // console.log(persons)
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
