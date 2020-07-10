643c9b2091e5286b741bd51d4b36fd304bbe81f6
Sean Yeo <seansqyeo@gmail.com>
2020-07-09 20:07:27 -0700

    finish 3.2

--- phonebook/index.js
+++ phonebook/index.js
@@ -28,6 +28,15 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const num_persons = persons.length
+    const date = new Date()
+    response.send(
+        `<p>Phonebook has info for ${num_persons} people</p>
+        <p>${date}</p>`
+        )
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
