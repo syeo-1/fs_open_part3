eb78d3a3a1fda95755c711d62b3e080d3b8209bf
Sean Yeo <seansqyeo@gmail.com>
2020-07-09 20:15:45 -0700

    finish 3.3

--- phonebook/index.js
+++ phonebook/index.js
@@ -28,6 +28,12 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    return person ? response.json(person) : response.status(404).end()
+})
+
 app.get('/info', (request, response) => {
     const num_persons = persons.length
     const date = new Date()
